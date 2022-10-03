7ccf4d2e6662305a829485fe6a518fe31d77bdce
Parth <parthvaghela791@gmail.com>
2022-10-03 12:24:54 +0530

    development

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./index.css"
+import App from "./App";
+ 
+/*What is the ReactDOM render?
+The ReactDOM. render() function takes two arguments, 
+HTML code and an HTML element. The purpose of the function
+ is to display the specified HTML code inside the specified HTML element. */ 
+/*What is the strict mode in React?
+StrictMode is a tool for highlighting potential problems in 
+an application. Like Fragment , StrictMode does not render 
+any visible UI. */
+ReactDOM.render(
+   <React.StrictMode>
+    <App/>
+    </React.StrictMode>
+ ,document.getElementById('root'))
